1833223757d45acf6c485ebc7bd7abac4d5e8494
alexlarbi <j0j0b@ugrad.cs.ubc.ca>
2020-05-12 11:28:22 -0700

    Theme Consumer

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
 import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
 
 class App extends React.Component {
   constructor(props) {
@@ -18,14 +19,22 @@ class App extends React.Component {
       }
     }
   }
-
   render() {
     return (
       <ThemeProvider value={this.state}>
-        <div className='container'>
-          <Battle />
+        <div className={this.state.theme}>
+          <div className='container'>
+            <Nav />
+
+            <Popular />
+          </div>
         </div>
       </ThemeProvider>
     )
   }
 }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
